432799b05e2b89a3a9248d0271b1d4de671619d0
GustavoAlfonsin <GustavoAriel.Alfonsin@alumnos.uai.edu.ar>
2024-05-12 17:08:01 -0300

    Cree al jugador y un arbol

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+
+        Vector3 velocity = Vector3.zero;
+
+        if (hor != 0 || ver != 0)
+        {
+            Vector3 direction = (transform.forward * ver + transform.right * hor).normalized;
+            velocity = direction * speed;
+        }
+        velocity.y = rb.velocity.y;
+        rb.velocity = velocity;
+    }
+}
